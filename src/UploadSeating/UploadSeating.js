2568246f422d010ef0da4dc33ff6a6eafd07134e
monish karunakaran <monish.karunakaran@quicksort.dev>
2021-03-21 18:43:23 +0530

    working modal feature

--- src/UploadSeating/UploadSeating.js
+++ src/UploadSeating/UploadSeating.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './header'
+import Modal from './Modal'
+
 
 export default function UploadSeating(props) {
     var user=props.location.state?.TID
     return (
         <div>
             <Header user={user} />
-            
+            <Modal/>
         </div>
     )
 }
