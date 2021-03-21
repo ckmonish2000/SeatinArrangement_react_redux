1855c1fdca8840827b985a4f3ae9dfd6bc7ff455
monish karunakaran <monish.karunakaran@quicksort.dev>
2021-03-21 19:07:18 +0530

    new reducer added event handlers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from "react-redux"
 import {createStore,combineReducers} from "redux"
-import {SignupReducer,LoginRedcerStudents,TeacherSignup,LoginTeacher,OpenNewModal} from "./utils/reducer"
+import {SignupReducer,LoginRedcerStudents,TeacherSignup,LoginTeacher,OpenNewModal,ModalValues} from "./utils/reducer"
 import {BrowserRouter} from "react-router-dom"
 
 
@@ -12,7 +12,8 @@ const allReducer=combineReducers({
   LoginRedcerStudents,
   TeacherSignup,
   LoginTeacher,
-  OpenNewModal
+  OpenNewModal,
+  ModalValues
 })
 
 const store=createStore(allReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
